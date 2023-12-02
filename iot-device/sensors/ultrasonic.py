cb38777a2caf29917d40d121d25f4dc1ffc44957
Gianmarco Psotka <gianmarco@umail.ucsb.edu>
2023-12-01 19:48:11 -0800

    getting started with ultrasonic sensor

--- iot-device/sensors/ultrasonic.py
+++ iot-device/sensors/ultrasonic.py
@@ -1,1 +1,12 @@
 #for ultrasonic distance sensor (HC-SR04)
+from gpiozero import DistanceSensor
+import time
+
+
+ultrasonic = DistanceSensor(echo=17, trigger=4)
+while True:
+    print(ultrasonic.distance)
+    ultrasonic.wait_for_in_range()
+    print("In range")
+    ultrasonic.wait_for_out_of_range()
+    print("Out of range")
