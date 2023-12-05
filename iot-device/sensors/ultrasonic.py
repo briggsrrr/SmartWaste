04d32848f190b2dbe54700bdbd1a3657f4585606
Gianmarco Psotka <gianmarco@umail.ucsb.edu>
2023-12-05 12:13:11 -0800

    edited ultrasonic sensor file

--- iot-device/sensors/ultrasonic.py
+++ iot-device/sensors/ultrasonic.py
@@ -1,12 +1,53 @@
 #for ultrasonic distance sensor (HC-SR04)
 from gpiozero import DistanceSensor
+import RPi.GPIO as GPIO
 import time
 
+# PIO mode
+GPIO.setmode(GPIO.BCM)
 
-ultrasonic = DistanceSensor(echo=17, trigger=4)
-while True:
-    print(ultrasonic.distance)
-    ultrasonic.wait_for_in_range()
-    print("In range")
-    ultrasonic.wait_for_out_of_range()
-    print("Out of range")
+# set GPIO pins
+GPIO_TRIGGER = 16
+GPIO_ECHO = 24
+
+# set GPIO direction
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # record StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # record time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    
+    TimeElapsed = StopTime - StartTime
+
+    # multiply with the ultrasonic speed (34300 cm/s) and divide by 2
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            print ("Measured Distance = %.1f cm" % dist)
+            time.sleep(10)
+ 
+    except KeyboardInterrupt:
+        GPIO.cleanup()
